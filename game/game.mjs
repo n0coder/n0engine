f691e8e0fb1aaa979454f1fad0de79b49fe77c7c
n0coder <uib@null.net>
2023-09-23 21:25:25 -0600

    new idea plan

--- game/game.mjs
+++ game/game.mjs
@@ -1,2 +1,26 @@
 //will find a worthy first game idea here
 //...
+
+//ok fishing game
+
+//click water
+//wait some time
+//dark circle will appear
+//click again
+//fish caught
+
+//place water
+//spawn circles
+//make circles move
+
+
+//have nano ais appear and run away with the fish
+//they give you money if you give them fish
+//sometimes evil nanos will try to poison the water
+//hire nano ais to defend them
+//they will slap the evil ais with their fish
+//the evil ais will go away
+
+//if you find sugary fish, you can turn evil ais good
+
+//(the nanoais are your friends and they will have their own lives)
