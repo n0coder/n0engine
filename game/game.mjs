713dfe0c6f13c640a9215729d9882ed9622b1ecc
n0coder <uib@null.net>
2023-09-23 21:11:02 -0600

    removed failed game attempt

--- game/game.mjs
+++ game/game.mjs
@@ -1,14 +1,2 @@
-import { cosmicEntityManager } from "../engine/core/CosmicEntity/CosmicEntityManager.mjs";
-import { Grid } from "../engine/grid/grid.mjs";
-import { SandGame } from "./SandGame.mjs";
-import { Sand } from "./sand.mjs";
-
-
-var autogame = new Grid(8, 224, 224, 0, 0);
-autogame.init();
-//gonna make a cellular automata game first
-var sandGame = new SandGame(autogame);
-cosmicEntityManager.addEntity(sandGame);
-var sand = new Sand(6, 0, autogame.tileSize);
-cosmicEntityManager.addEntity(sand);
-sandGame.addSand(sand)
+//will find a worthy first game idea here
+//...
