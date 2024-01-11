64dfc5666d20ce697c80e8e46e78a1b505ef720a
n0coder <uib@null.net>
2024-01-11 02:07:50 -0700

    exploring hooks and traps...

--- game/jobSystem.mjs
+++ game/jobSystem.mjs
@@ -4,21 +4,24 @@ import { Nanoai } from "./nanoai/nanoai.mjs";
 import { n0radio } from "./radio/n0radio.mjs";
 import { nanoaiActions } from "./nanoai/nanoaiActions.mjs";
 import { createJobu, jobTasksa } from "./radio/jobSystem.mjs";
+import { cosmicEntityManager } from "../engine/core/CosmicEntity/CosmicEntityManager.mjs";
+import { p } from "../engine/core/p5engine.mjs";
+import { deltaTime } from "../engine/core/Time/n0Time.mjs";
 
 let n0 = new Nanoai("n0", 200,256); 
-let abi = new Nanoai("abi", 256,256); 
+let abi = new Nanoai("abi", 456,500); 
 
 
 
 //i have a funny idea: nanoai tictactoe
-
+/*
 let game = {
     players: [], playerCount: 2,
     play: function(nano) {
         console.log(`${nano.name} nano joining tictactoe`);
     }
 }
-
+*/
 /*
 "smile", function(...args) {
         return {
@@ -93,10 +96,12 @@ jobTasksa.set("play", function(...args) { //this is singleplayer play function p
         }
     }
 })
+/*
 var gamejob = createJobu(game, "playtogether")
 n0radio.postJob("general", gamejob);
 
-var gamezjob = createJobu(game, "play")
+var gamezjob = createJobu(game, "play");
+*/
 //n0radio.postJob("general", gamezjob); //we've been working on jobs with multiple tasks so much it didn't occur to me to ignore jobs with stages empty
 
 //ok, so, jobs need another multiplier
@@ -134,12 +139,113 @@ line of thinking:
     the tictactoe board does the controlling of the game; we're pretending that the nanos are doing all the work themselves
 */
 
+/*
 
+nano: "i want to work" *radio beeps, they got a job*
+nano: "yay i'm working" *does work*
+nano: "work is over :(" *dissapointed work ended already*
+*radio confused* (this is a multiworker job the nano didn't do the work)
+we needed this nano to enter an activity trap so the nano isn't disapointed prematurely
+
+we have a common ground, so here's my solution to that issue above:
+
+  
+  this is how we define traphooks:
+  
+
+ //the traphook is an object with a function inside so that a nano stays in it's current state untill the hook is pulled (until the function is called)
+  
+this means that the activitytrap will return true until the game calls the hook then it will return false (this exits this state in the nanos state machine etc)
+*/
+
+class TestGame {
+    constructor() {
+        this.nanos = [], this.timer = 0
+    }
+    draw() {
+        let size = 32;
+        p.fill(128, 135,88)
+        if (this.nanos.length > 1){
+            size = 64;
+            p.fill(200, 255,200)
+            if (this.nanos.every(n=>n[1].pull !== null)) {
+                size = 64;
+                p.fill(128, 185,128)
+                this.timer += deltaTime;
+                if (this.timer > 3) {
+                    this.timer = 0;
+                this.nanos = this.nanos.filter((nn) => {
+                    if (nn[1].pull !== null) {
+                      nn[1].pull(this);
+                      return false; // exclude this nano from the new array
+                    }
+                    return true; // include this nano in the new array
+                });
+                }
+            }
+            
+        }
+            
+        p.ellipse(356, 156, size, size);
+    }
+    addNano(nano, traphook) { 
+        this.nanos.push([nano, traphook]); 
+    }
+}
+let game = new TestGame();
+cosmicEntityManager.addEntity(game)
+/*
+let game = { nanos: [], 
+    update: function() { 
+       // wait until everyone is accounted for  
+       // do logic, play with the nanos 
+       // call the nanos traphook pull function to exit the trap, then remove the nano from the list
+       if (nanos.length > 0) {
+            this.
+         this.nanos.pop();
+       }
+      },
+      addNano: function(nano, traphook) { this.nanos.push([nano, traphook]); }
+    }
+*/
+let traphook ={pull: null}, traphook2 = {pull: null};
+game.addNano(n0, traphook); game.addNano(abi, traphook2);
+n0.brain.do("walk", 356, 156); abi.brain.do("walk", 356, 176); 
+n0.brain.doTask({ 
+    args: [traphook], hook: null, okok: true,
+    work: function (nano) { //this is called every frame until we return false
+        if (this.hook === null) {
+            this.hook = this.args[1] ?? "a"
+        }
+
+        this.args[1].pull = (d) => {
+            console.log("traphook was pulled", d)
+            this.okok = false;
+        }
+        return this.okok;
+    }        
+}, ()=>{"done"})
+abi.brain.doTask({ 
+    args: [traphook2], hook: null, okok: true,
+    work: function (nano) { //this is called every frame until we return false
+        if (this.hook === null) {
+            this.hook = this.args[1] ?? "a"
+        }
+
+        this.args[1].pull = (d) => {
+            console.log("traphook was pulled", d)
+            this.okok = false;
+        }
+        return this.okok;
+    }        
+}, ()=>{"done"})
+n0.brain.do("walk", 111, 111); abi.brain.do("walk", 111, 131)
+//n0.brain.doTask(activityTrap)
 let circle4 = new Circle(7,2, 8,8);
 let circle5 = new Circle(64,128, 8,8);
 let circle6 = new Circle(128,128, 8,8);
-n0radio.findJob(n0); //naturally this would make them start working if a job is available, but here there's no job just yet
-n0radio.findJob(abi); 
+//n0radio.findJob(n0); //naturally this would make them start working if a job is available, but here there's no job just yet
+//n0radio.findJob(abi); 
 
 //let job2 = createJobu([circle4, circle5, circle6], "smile", "hi"); 
 //n0radio.postJob("general", job2);  //the job post will tell the ais to start working
