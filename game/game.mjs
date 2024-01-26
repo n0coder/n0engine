1fb46186d21f8d50077c1916178a5d255bfe5c19
n0coder <uib@null.net>
2024-01-25 19:22:32 -0700

    a plan for rebuilding world generator settings

--- game/game.mjs
+++ game/game.mjs
@@ -1,1 +1,4 @@
-//overwhelmed, need a better space to focus on jobs systems
+// we need to form a basic world generation layout for the nanos, something simple and cute
+
+// form based on the nanos size, a realtive difficulty to find water, which will then change based on humidity and temperature
+// we need a solid world size, then we should form the biome noises. tough challenge
