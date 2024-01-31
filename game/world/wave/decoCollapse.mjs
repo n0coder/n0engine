cde6898f6515908a69062baa83de41554dae4dd2
n0coder <uib@null.net>
2024-01-30 20:37:30 -0700

    deco collapse ideas

---
+++ game/world/wave/decoCollapse.mjs
@@ -0,0 +1,7 @@
+//import noise from some map
+//import biomes from that map
+//import tile sprites from those biomes
+//paint those tiles above the map using n0functioncollapse techs
+
+//note air as the medium of control for allowing other decorations to also exist next to them. 
+// 0 is the id for air or something
